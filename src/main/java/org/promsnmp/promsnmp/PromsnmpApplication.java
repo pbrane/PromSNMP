d250043cc70eb919881c4cffdd592eefbf8ba975
David Hustace <david@beaconstrategists.com>
2025-02-24 09:30:45 -0500

    first commit; actuator data exposed with prometheus

---
+++ src/main/java/org/promsnmp/promsnmp/PromsnmpApplication.java
@@ -0,0 +1,13 @@
+package org.promsnmp.promsnmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromsnmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromsnmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/promsnmp/promsnmp/PromsnmpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.promsnmp.promsnmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromsnmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
