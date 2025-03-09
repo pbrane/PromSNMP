787321683f7af8d73191378d284c7dfcc1c0aa85
David Hustace <david@beaconstrategists.com>
2025-03-09 09:43:26 -0400

    bump version for docker push

--- src/main/java/org/promsnmp/promsnmp/controllers/DemoController.java
+++ src/main/java/org/promsnmp/promsnmp/controllers/DemoController.java
@@ -1,13 +1,12 @@
 package org.promsnmp.promsnmp.controllers;
 
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
