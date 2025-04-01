dca1b92a155308c55d771406c348c23ef1702e34
David Hustace <david@beaconstrategists.com>
2025-04-01 07:03:25 -0400

    rename service api configuration class

--- src/main/java/org/promsnmp/promsnmp/configuration/ServiceApiConfig.java
+++ src/main/java/org/promsnmp/promsnmp/configuration/ServiceApiConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ServiceConfig {
+public class ServiceApiConfig {
 
     @Value("${SERVICE_API:demo}")
     private String apiSvcMode;
