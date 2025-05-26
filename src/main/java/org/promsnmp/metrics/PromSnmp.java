bcde2c2b6a90e1c094d815d1686ba19e13a4a2a2
David Hustace <david@beaconstrategists.com>
2025-05-26 10:26:01 -0400

    Add scanning annotations to support commons package

--- src/main/java/org/promsnmp/metrics/PromSnmp.java
+++ src/main/java/org/promsnmp/metrics/PromSnmp.java
@@ -2,7 +2,9 @@ package org.promsnmp.metrics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 @EnableScheduling
 @EnableAsync
+@ComponentScan(basePackages = { "org.promsnmp.metrics", "org.promsnmp.common" })
+@EntityScan(basePackages = { "org.promsnmp.common.model" })
 public class PromSnmp {
 
 	public static void main(String[] args) {
